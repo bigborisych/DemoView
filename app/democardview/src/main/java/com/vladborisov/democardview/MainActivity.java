e5a35ff735291f55b48bf691d4674a00eec26608
Borisov Vlad <borisov.vlad27@gmail.com>
2021-04-19 17:54:42 +0300

    Create Card View, Create new module Text Input Layout Demo

--- app/democardview/src/main/java/com/vladborisov/democardview/MainActivity.java
+++ app/democardview/src/main/java/com/vladborisov/democardview/MainActivity.java
@@ -1,14 +1,29 @@
 package com.vladborisov.democardview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ConstraintLayout cl = new ConstraintLayout(this);
+        setContentView(cl);
+        View cardView = LayoutInflater.from(this).inflate(R.layout.content_main, cl, false);
+        cl.addView(cardView);
     }
 }
