ed8da41b08ca7dc615e62ff8c216c9635692f8ff
Borisov Vlad <borisov.vlad27@gmail.com>
2021-04-20 16:14:24 +0300

    Added DemoCardView activity.

--- app/src/main/java/com/vladborisov/demoview/ActivityMain.java
+++ app/src/main/java/com/vladborisov/demoview/ActivityMain.java
@@ -1,6 +1,8 @@
 package com.vladborisov.demoview;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,15 @@ public class ActivityMain extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.demo_card_view_button) {
+            Intent intent = new Intent(this, DemoCardView.class);
+            startActivity(intent);
+        } else if (view.getId() == R.id.text_input_layout_button) {
+            Intent intent = new Intent(this, TextInputLayout.class);
+            startActivity(intent);
+        }
     }
 }
