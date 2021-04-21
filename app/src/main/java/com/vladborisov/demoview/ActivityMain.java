8f23957ff525dc428bc7440ac1e67b029b0f7030
Borisov Vlad <borisov.vlad27@gmail.com>
2021-04-21 22:16:14 +0300

    Create nav drawer.

--- app/src/main/java/com/vladborisov/demoview/ActivityMain.java
+++ app/src/main/java/com/vladborisov/demoview/ActivityMain.java
@@ -2,26 +2,87 @@ package com.vladborisov.demoview;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 
-public class ActivityMain extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
-
-    public void onClick(View view) {
-        Intent intent = new Intent(this, DemoView.class);
-        if (view.getId() == R.id.demo_card_view_button) {
-            intent.putExtra("View", 1);
-            startActivity(intent);
-        } else if (view.getId() == R.id.text_input_layout_button) {
-            intent.putExtra("View", 2);
-            startActivity(intent);
+        public void onClick (View view){
+            Intent intent = new Intent(this, DemoView.class);
+            if (view.getId() == R.id.demo_card_view_button) {
+                intent.putExtra("View", 1);
+                startActivity(intent);
+            } else if (view.getId() == R.id.text_input_layout_button) {
+                intent.putExtra("View", 2);
+                startActivity(intent);
+            }
+        }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_tools) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    }
